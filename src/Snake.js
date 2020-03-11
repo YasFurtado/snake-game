342b0599139530a053124ce1a2c456be695ebfd8
Yasmim Furtado <40840835+YasFurtado@users.noreply.github.com>
2020-03-11 10:17:28 -0300

    alteração no "const" da cobra e da comida

--- src/Snake.js
+++ src/Snake.js
@@ -5,8 +5,8 @@ export default (props) => {
         <div>
             {props.snakeDots.map((dot, i) => {
                 const style = {
-                    left:'${dot[0]}%',
-                    top:'${dot[1]}%'
+                    left: `${dot[0]}%`,
+                    top: `${dot[1]}%`
                 }
                 return (
                     <div className="snake-dot" key={i} style={style}></div> 
