b3f8476db17d267effb07cc147944229d48d5b62
Yasmim Furtado <40840835+YasFurtado@users.noreply.github.com>
2020-03-11 10:00:25 -0300

    correção de cálculo de posição

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Food from './Food';
 const getRandomCoordinates = () => {
   let min= 1;
   let max = 98;
-  let x = Math.floor((Math.random()= (max-min+1)+min)/2)*2;
-  let y = Math.floor((Math.random()= (max-min+1)+min)/2)*2;
+  let x = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
+  let y = Math.floor((Math.random()*(max-min+1)+min)/2)*2;
   return [x,y]
 }
 class App extends Component{
