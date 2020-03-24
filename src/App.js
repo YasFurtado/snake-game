5fe2eadac627db4fd1c471d6dcd12bc0dcf75e14
Yasmim Furtado <furtadommoura@gmail.com>
2020-03-24 14:45:50 -0300

    movimento da cobrinha

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component{
 
   state = {
     food: getRandomCoordinates (),
+    speed:200,
     direction: 'RIGHT',
     snakeDots: [
       [0,0],
@@ -21,7 +22,12 @@ class App extends Component{
   }
 
   componentDidMount(){
+    setInterval(this.moveSnake, this.state.speed);
     document.onkeydown = this.onKeyDown;
+  }
+
+  componentDidUpdate(){
+    this.checkIfOutOfBorders()
   }
 
   onKeyDown = (e) => {
@@ -39,9 +45,46 @@ class App extends Component{
       case 39:
         this.setState({direction: 'RIGHT'});
         break;
-
     }
   }
+
+  moveSnake = () => {
+    let dots = [...this.state.snakeDots]
+    let head = dots[dots.length - 1];
+
+    switch (this.state.direction) {
+      case 'RIGHT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'LEFT':
+        head = [head[0] + 2, head[1]];
+        break;
+      case 'DOWN':
+        head = [head[0], head[1] + 2];
+        break;
+      case 'UP':
+        head = [head[0], head[1] - 2];
+        break;
+    }
+    dots.push(head);
+    dots.shift();
+    this.setState({
+      snakeDots: dots
+    })
+  }
+
+  checkIfOutOfBorders(){
+    let head = this.state.snakeDots[this.state.snakeDots.length - 1];
+    if (head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1] < 0){
+      this.onGameOver();
+    }
+  }  
+
+  onGameOver(){
+    alert( `Game over. Snake length is ${this.state.snakeDots.length}`);
+
+  }
+
   render() {
     return (
       <div className="game-area">
